7c376db45622ba00023499b34365e31a83b07f2d
Nguyen-Dinh-Sang <nguyendinhsang9199@gmail.com>
2022-08-07 21:55:02 +0700

    first commit

---
+++ src/main/java/magicofspring/SpringMagicOfJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package magicofspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMagicOfJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMagicOfJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/magicofspring/SpringMagicOfJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package magicofspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMagicOfJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
