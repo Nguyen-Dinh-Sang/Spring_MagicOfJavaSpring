93be4cbd310c43cd8051facae45c7f96b2ad27b7
Nguyen-Dinh-Sang <nguyendinhsang9199@gmail.com>
2022-08-12 00:56:01 +0700

    SpringInDepth-SangND: Lifecycle of a Bean.

--- src/main/java/magicofspring/springindepth/basic/BinarySearchImpl.java
+++ src/main/java/magicofspring/springindepth/basic/BinarySearchImpl.java
@@ -1,10 +1,15 @@
 package magicofspring.springindepth.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 // @Scope("prototype") is not really a good practice ->
@@ -15,10 +20,23 @@ public class BinarySearchImpl {
     // Priority: @Qualifier > @Primary > name
     private SortAlgorithm sortAlgorithm;
 
+    private Logger LOGGER = LoggerFactory.getLogger(BinarySearchImpl.class);
+
     public int binarySearch(int[] numbers, int numberToSearchFor) {
         int[] sortedNumbers = sortAlgorithm.sort(numbers);
         System.out.println(sortAlgorithm);
         // Search the array.
         return 3;
     }
+
+    @PostConstruct
+    private void postConstruct() {
+        LOGGER.info("postConstruct");
+    }
+
+    @PreDestroy
+    private void preDestroy() {
+        LOGGER.info("preDestroy");
+    }
+
 }
