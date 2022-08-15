f1bffe5645033c6f328bb42be87d561a6c22cc66
Nguyen-Dinh-Sang <nguyendinhsang9199@gmail.com>
2022-08-15 10:49:38 +0700

    UnitTesting-SangND: Important annotations.

--- src/test/java/magicofspring/unittesting/MyMathTest.java
+++ src/test/java/magicofspring/unittesting/MyMathTest.java
@@ -1,16 +1,37 @@
 package magicofspring.unittesting;
 
-import org.junit.jupiter.api.Test;
+import org.junit.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-class MyMathTest {
+public class MyMathTest {
 
     MyMath myMath = new MyMath();
 
+    @Before
+    public void before() {
+        System.out.println("Before");
+    }
+
+    @After
+    public void after() {
+        System.out.println("After");
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Before Class");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("After Class");
+    }
+
     // 1, 2, 3 => 6
     @Test
-    void sumWith3Numbers() {
+    public void sumWith3Numbers() {
+        System.out.println("Test1");
         int result = myMath.sum(new int[] {1, 2, 3});
         // Check that the result is 6
 
@@ -18,7 +39,8 @@ class MyMathTest {
     }
 
     @Test
-    void sumWith1Numbers() {
+    public void sumWith1Numbers() {
+        System.out.println("Test2");
         assertEquals(3, myMath.sum(new int[] {3}));
     }
 }
