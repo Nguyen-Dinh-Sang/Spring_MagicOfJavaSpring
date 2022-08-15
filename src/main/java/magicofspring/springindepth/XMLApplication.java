ba016d716f5b77552ccdb2df0fb120f3979975f7
Nguyen-Dinh-Sang <nguyendinhsang9199@gmail.com>
2022-08-15 08:55:45 +0700

    SpringInDepth-SangND: Mixing XML Context with Component Scan.

--- src/main/java/magicofspring/springindepth/XMLApplication.java
+++ src/main/java/magicofspring/springindepth/XMLApplication.java
@@ -1,14 +1,15 @@
 package magicofspring.springindepth;
 
 import magicofspring.springindepth.xml.XMLPersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @Configuration
-@ComponentScan
 public class XMLApplication {
+	private static Logger LOGGER = LoggerFactory.getLogger(XMLApplication.class);
 
 	public static void main(String[] args) {
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml")) {
@@ -18,6 +19,9 @@ public class XMLApplication {
 
 	private static void getBeanContext(ApplicationContext context) {
 		XMLPersonDAO xmlPersonDAO = context.getBean(XMLPersonDAO.class);
+
+		// List bean definition
+		LOGGER.info("Bean Loaded -> {}", (Object) context.getBeanDefinitionNames());
 
 		System.out.println(xmlPersonDAO);
 		System.out.println(xmlPersonDAO.getXmlJdbcConnection());
