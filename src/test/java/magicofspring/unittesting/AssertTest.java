6dcdc2e818a76faafa9a8efc5bcbcdfd70049b8d
Nguyen-Dinh-Sang <nguyendinhsang9199@gmail.com>
2022-08-15 10:35:46 +0700

    UnitTesting-SangND: Assert methods.

---
+++ src/test/java/magicofspring/unittesting/AssertTest.java
@@ -0,0 +1,30 @@
+package magicofspring.unittesting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssertTest {
+
+    @Test
+    public void testCase1() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void testCase2() {
+        assertTrue(1 + 1 == 2);
+    }
+
+    @Test
+    public void testCase3() {
+        assertFalse(1 + 1 == 3);
+    }
+
+    @Test
+    public void testCase4() {
+        assertNull(null);
+        assertNotNull(null);
+        assertArrayEquals(new int[] {1, 2, 4}, new int[] {1, 2, 4});
+    }
+}
