0aaafaebac31fa4e53438016e5386a0f91977220
Nguyen-Dinh-Sang <nguyendinhsang9199@gmail.com>
2022-08-16 14:51:09 +0700

    SpringBoot-SangND: Exploring Spring Boot Actuator.

--- src/main/java/magicofspring/springboot/courses/controller/CourseController.java
+++ src/main/java/magicofspring/springboot/courses/controller/CourseController.java
@@ -9,8 +9,17 @@ import java.util.List;
 
 @RestController
 public class CourseController {
+    // http://localhost:8080/courses
     @GetMapping("/courses")
-    public List<Course> getAllCourse() {
-        return Arrays.asList(new Course(1, "Learn Microservices", "in28minutes"));
+    public List<Course> getAllCourses() {
+        return Arrays.asList(new Course(1, "Learn Microservices", "in28minutes"),
+                new Course(2, "Learn Full Stack with Angular and React", "in28minutes"));
     }
+
+    // http://localhost:8080/courses/1
+    @GetMapping("/courses/1")
+    public Course getAllCourseDetails() {
+        return new Course(1, "Learn Microservices", "in28minutes");
+    }
+
 }
