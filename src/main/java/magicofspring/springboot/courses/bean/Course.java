cea0eed9374462d095d12d1c4c2c995de891cbcc
Nguyen-Dinh-Sang <nguyendinhsang9199@gmail.com>
2022-08-16 13:56:19 +0700

    SpringBoot-SangND: Build hello world.

---
+++ src/main/java/magicofspring/springboot/courses/bean/Course.java
@@ -0,0 +1,12 @@
+package magicofspring.springboot.courses.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Course {
+    private long id;
+    private String name;
+    private String author;
+}
