32aa375d50adfe4d963f1aeed122332cc0159360
Nguyen-Dinh-Sang <nguyendinhsang9199@gmail.com>
2022-08-17 11:41:28 +0700

    AOP-SangND: Use common Pointcuts Configuration.

--- src/main/java/magicofspring/aop/aspect/CommonJoinPointConfig.java
+++ src/main/java/magicofspring/aop/aspect/CommonJoinPointConfig.java
@@ -5,4 +5,19 @@ import org.aspectj.lang.annotation.Pointcut;
 public class CommonJoinPointConfig {
     @Pointcut("execution(* magicofspring.aop.data.*.*(..))")
     public void dataLayerExecution() {}
+
+    @Pointcut("execution(* magicofspring.aop.business.*.*(..))")
+    public void businessLayerExecution() {}
+
+    @Pointcut("magicofspring.aop.aspect.CommonJoinPointConfig.dataLayerExecution() && magicofspring.aop.aspect.CommonJoinPointConfig.businessLayerExecution()")
+    public void allLayerExecution() {}
+
+    @Pointcut("bean(*dao*)")
+    public void beanContrainingDao() {}
+
+    @Pointcut("bean(dao*)")
+    public void beanStartingWithDao() {}
+
+    @Pointcut("within(magicofspring.aop.data..*)")
+    public void dataLayerExecutionWithWithin() {}
 }
