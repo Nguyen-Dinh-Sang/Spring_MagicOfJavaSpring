6767fd9f4354361785cfa659c315d7ac5e1a8eb4
Nguyen-Dinh-Sang <nguyendinhsang9199@gmail.com>
2022-08-18 14:22:11 +0700

    JDBCToJPA-SangND: Setting up a project.

--- src/main/java/magicofspring/aop/AOPApplication.java
+++ src/main/java/magicofspring/aop/AOPApplication.java
@@ -1,32 +1,32 @@
-package magicofspring.aop;
-
-import magicofspring.aop.business.Business1;
-import magicofspring.aop.business.Business2;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class AOPApplication implements CommandLineRunner {
-
-    @Autowired
-    private Business1 business1;
-
-    @Autowired
-    private Business2 business2;
-
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    public static void main(String[] args) {
-        SpringApplication.run(AOPApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        logger.info(business1.calculateSomeThing());
-        logger.info(business2.calculateSomeThing());
-    }
-}
+//package magicofspring.aop;
+//
+//import magicofspring.aop.business.Business1;
+//import magicofspring.aop.business.Business2;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class AOPApplication implements CommandLineRunner {
+//
+//    @Autowired
+//    private Business1 business1;
+//
+//    @Autowired
+//    private Business2 business2;
+//
+//    private Logger logger = LoggerFactory.getLogger(this.getClass());
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(AOPApplication.class, args);
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        logger.info(business1.calculateSomeThing());
+//        logger.info(business2.calculateSomeThing());
+//    }
+//}
--- src/test/java/magicofspring/mockito/BusinessStubTest.java
+++ src/test/java/magicofspring/mockito/BusinessStubTest.java
@@ -1,24 +1,24 @@
-package magicofspring.mockito;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class BusinessStubTest {
-
-    @Test
-    void findTheGreatestFromAllData() {
-        BusinessImpl business = new BusinessImpl(new DataServiceStub());
-        int result = business.findTheGreatestFromAllData();
-        assertEquals(23, result);
-    }
-}
-
-// Have to write a lot of code like this to maintain this.
-class DataServiceStub implements DataService {
-
-    @Override
-    public int[] retrieveAllDate() {
-        return new int[] {23, 4, 5};
-    }
-}
+//package magicofspring.mockito;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class BusinessStubTest {
+//
+//    @Test
+//    void findTheGreatestFromAllData() {
+//        BusinessImpl business = new BusinessImpl(new DataServiceStub());
+//        int result = business.findTheGreatestFromAllData();
+//        assertEquals(23, result);
+//    }
+//}
+//
+//// Have to write a lot of code like this to maintain this.
+//class DataServiceStub implements DataService {
+//
+//    @Override
+//    public int[] retrieveAllDate() {
+//        return new int[] {23, 4, 5};
+//    }
+//}
