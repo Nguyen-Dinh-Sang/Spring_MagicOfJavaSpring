401892ad5fb1065926c241262602f8ad69c230cb
Nguyen-Dinh-Sang <nguyendinhsang9199@gmail.com>
2022-08-28 21:42:53 +0700

    JDBCToJPA-SangND: Implementing update, insert.

--- src/main/java/magicofspring/jdbctojpa/DataApplication.java
+++ src/main/java/magicofspring/jdbctojpa/DataApplication.java
@@ -1,12 +1,15 @@
 package magicofspring.jdbctojpa;
 
 import magicofspring.jdbctojpa.dao.PersonJdbcDao;
+import magicofspring.jdbctojpa.entity.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class DataApplication implements CommandLineRunner {
@@ -24,5 +27,8 @@ public class DataApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         logger.info("All users -> {}", personJdbcDao.findAll());
         logger.info("User id 10001 -> {}", personJdbcDao.findById(10001));
+        logger.info("Deleting id 10002 -> No of row deleted - {}", personJdbcDao.deleteById(10002));
+        logger.info("Inserting id 10004 -> {}", personJdbcDao.insert(new Person(10004, "Tara", "Berlin", new Date())));
+        logger.info("Update 10003 - {}", personJdbcDao.update(new Person(10003, "Pieter", "Utrecht", new Date())));
     }
 }
--- src/main/java/magicofspring/jdbctojpa/entity/Person.java
+++ src/main/java/magicofspring/jdbctojpa/entity/Person.java
@@ -1,10 +1,14 @@
 package magicofspring.jdbctojpa.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     private int id;
     private String name;
