fa767738dec3056ebb33a171b5e4f403f25effa3
Nguyen-Dinh-Sang <nguyendinhsang9199@gmail.com>
2022-08-28 20:39:47 +0700

    JDBCToJPA-SangND: Implementing findAll, findById.

--- src/main/java/magicofspring/jdbctojpa/DataApplication.java
+++ src/main/java/magicofspring/jdbctojpa/DataApplication.java
@@ -1,11 +1,28 @@
 package magicofspring.jdbctojpa;
 
+import magicofspring.jdbctojpa.dao.PersonJdbcDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataApplication {
+public class DataApplication implements CommandLineRunner {
+
+    @Autowired
+    private PersonJdbcDao personJdbcDao;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public static void main(String[] args) {
         SpringApplication.run(DataApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("All users -> {}", personJdbcDao.findAll());
+        logger.info("User id 10001 -> {}", personJdbcDao.findById(10001));
     }
 }
---
+++ src/main/java/magicofspring/jdbctojpa/dao/PersonJdbcDao.java
@@ -0,0 +1,23 @@
+package magicofspring.jdbctojpa.dao;
+
+import magicofspring.jdbctojpa.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJdbcDao {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person findById(int id) {
+        return jdbcTemplate.queryForObject("select * from person where id = ?", new Object[] {id}, new BeanPropertyRowMapper<>(Person.class));
+    }
+}
