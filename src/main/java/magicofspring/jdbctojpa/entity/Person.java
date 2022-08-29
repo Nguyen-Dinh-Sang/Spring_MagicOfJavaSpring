15033a718ff035d473d6a33a16aab37eb05e1b1c
Nguyen-Dinh-Sang <nguyendinhsang9199@gmail.com>
2022-08-29 13:33:50 +0700

    JDBCToJPA-SangND: Defining Person Entity.

--- src/main/java/magicofspring/jdbctojpa/entity/Person.java
+++ src/main/java/magicofspring/jdbctojpa/entity/Person.java
@@ -4,14 +4,27 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue
     private int id;
+
     private String name;
     private String location;
     private Date birthDate;
+
+    public Person(String name, String location, Date birthDate) {
+        this.name = name;
+        this.location = location;
+        this.birthDate = birthDate;
+    }
 }
